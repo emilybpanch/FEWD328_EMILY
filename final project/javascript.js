89aeb823910addcac526e0e01d24ac9ca35c447c
emilybpanch <emilybpanchame@gmail.com>
2017-05-22 14:29:06 -0400

    Final project - 5.22.17

--- final project/javascript.js
+++ final project/javascript.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
-  $('.slideshow').slick({
+
+  /*show header on scroll down; surface on scroll up*/
+
+$(function(){
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('.navbar').outerHeight();
+  
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+  
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+  
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+      return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+      // Scroll Down
+      $('.navbar').removeClass('nav-down').addClass('nav-up');
+    } else {
+      // Scroll Up
+      if(st + $(window).height() < $(document).height()) {
+        $('.navbar').removeClass('nav-up').addClass('nav-down');
+      }
+    }
+    
+    lastScrollTop = st;
+  }
+  });
+
+/*slick slideshow*/
+
+  $(".slideshow").slick({
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
@@ -24,19 +70,47 @@ $(document).ready(function(){
     }
   ]
 });
-        
+});
+
+/*Anchor top nav items to city openers
 
 
 
-/*  function initMap() {
-    var uluru = {lat: 35.725987, lng: 139.730386};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 5,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
   });
-};
-})*/
+
+*/
+     
